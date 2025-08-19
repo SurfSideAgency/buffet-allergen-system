ff1485262af7ffff702f79b493e9f5d52f1c90bd
SurfSide Agency <administracion@surfsidemkt.com>
2025-08-19 18:38:56 +0200

    Update server.js
    Fix static file serving in Vercel

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const openai = new OpenAI({
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Base de datos en memoria para el MVP
 let dishes = [];
